1121b0b59d183c735fc9e67b2dcfc86e855d2fdf
Minato86 <doronomori660@outlook.jp>
2020-11-02 22:49:53 +0900

    add: フッター作った

--- pages/index.vue
+++ pages/index.vue
@@ -7,20 +7,33 @@
             class="top-title text-white"
         >
         </b-jumbotron>
-        <b-container>
+        <b-container class="py-4">
             <h1>Services</h1>
-            <a href="https://tokyounderground.net/" target="_blank">楽曲プロモーションレーベル - TokyoUnderground (2020-)</a>
+            <a href="https://tokyounderground.net/" target="_blank"
+                >楽曲プロモーションレーベル - TokyoUnderground (2020-)</a
+            >
+            <h1 class="mt-5">Software</h1>
         </b-container>
+        <div class="footer mt-5 py-3">
+            <b-container>
+                <p>&copy; TeamOpenWorld 2020</p>
+                <a href="https://twitter.com/openworld_net" target="_blank">Twitter</a>
+            </b-container>
+        </div>
     </div>
 </template>
 
 <style scoped>
 .top-title {
-    background: linear-gradient(-135deg, #E4A972, #9941D8);
-    font-family: 'Noto Sans JP'
+    background: linear-gradient(-135deg, #e4a972, #9941d8);
+    font-family: "Noto Sans JP";
 }
 
 a {
-    font-family: 'Noto Sans JP'
+    font-family: "Noto Sans JP";
+}
+
+.footer {
+    border-top:2px solid black;
 }
 </style>
