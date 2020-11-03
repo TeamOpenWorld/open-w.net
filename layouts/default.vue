1ef0de6edf304fb0b9e1cf1fff58f7b9e5b38e98
Minato86 <doronomori660@outlook.jp>
2020-11-03 20:12:59 +0900

    add: グローバルナビの実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,38 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+    <div>
+        <section class="nav">
+            <a href="">楽曲レーベル</a>
+        </section>
+        <Nuxt />
+    </div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<style scoped>
+p {
+    padding: 0;
+    margin: 0;
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+.nav {
+    padding: 0 1em;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.nav > a {
+    text-decoration: none;
+    padding: 0.5em;
+    display: block;
+    background: linear-gradient(to right, #37aed0 50%, #fff 50%);
+    background-size: 200% 100.5%;
+    background-position: right bottom;
+    font-size: 0.9em;
+    color: #020202;
+    border: none;
+    border-color: none;
+    transition: all 0.2s ease-out;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.nav > a:hover {
+    background-position: left bottom;
+    color: white;
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
     'nuxt-webfontloader',
+    '@nuxtjs/axios'
   ],
 
   webfontloader: {
