9435809e62014c52990545e29400021b7189641a
Minato86 <doronomori660@outlook.jp>
2020-11-30 01:01:23 +0900

    fix: ヘッダーの最適化

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <iframe src="https://teamopenworld.github.io/openworld-header/" width="100%" frameborder="0"></iframe>
+        <iframe src="https://teamopenworld.github.io/openworld-header/" width="100%" height="45px" frameborder="0" scrolling="no"></iframe>
         <Nuxt />
     </div>
 </template>
